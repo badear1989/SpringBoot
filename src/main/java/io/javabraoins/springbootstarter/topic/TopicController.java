9f425726b67a5b9cbbdd258c75235272519b608f
Razvan Lenovo <badear1989@gmail.com>
2018-11-05 18:01:46 +0200

    added template - WIP

--- src/main/java/io/javabraoins/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabraoins/springbootstarter/topic/TopicController.java
@@ -3,8 +3,6 @@ package io.javabraoins.springbootstarter.topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
